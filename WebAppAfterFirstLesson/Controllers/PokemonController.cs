06811e9c50b9047b6bc79005777741f471a171af
andkovs <andrey.m.kovshov@gmail.com>
2016-03-02 20:57:32 +0300

    PokemonApp for ASP.Net masterclass

---
+++ WebAppAfterFirstLesson/Controllers/PokemonController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppAfterFirstLesson.Models;
+
+namespace WebAppAfterFirstLesson.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+            PokemonModels[] pokemons = {
+                                           new PokemonModels{PokemonName="Pikachu", MasterName="Ash", PicUrl="pikachu.jpg"},
+                                           new PokemonModels{PokemonName="Bulbasaur", MasterName="Ash", PicUrl="bulbasaur.jpg"},
+                                           new PokemonModels{PokemonName="Vulpix", MasterName="Broke", PicUrl="vulpix.jpg"},
+                                           new PokemonModels{PokemonName="Psyduck", MasterName="Misty", PicUrl="psyduck.jpg"}
+                                       };
+
+
+            return View(pokemons);
+        }
+    }
+}
---
+++ WebAppAfterFirstLesson/Models/PokemonModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppAfterFirstLesson.Models
+{
+    public class PokemonModels
+    {
+        public String PokemonName { get; set; }
+        public String MasterName { get; set; }
+        public String PicUrl { get; set; }
+    }
+}
